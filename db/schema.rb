6cd0b12d188446a1be90528e0079efee02505c97
alexoviedo999 <alexoviedo999@gmail.com>
2012-10-22 14:45:04 -0400

    test scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121022181039) do
+ActiveRecord::Schema.define(:version => 20121022183955) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "songs", :force => true do |t|
     t.string   "name"
