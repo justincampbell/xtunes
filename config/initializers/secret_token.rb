a7dc7775c7b3ebc0b498db7d4f7eaa8500cda2c3
alexoviedo999 <alexoviedo999@gmail.com>
2012-10-22 10:18:24 -0400

    Initial generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xtunes::Application.config.secret_token = '33e3ab0e4f7b5c665b09e5980786cea17802a5c040aed4ff5c31970f9037e83983096611a696f8ddb0ec9c5bd50a5a85c19143d73bf2f8749704b69b89b39d7e'
