55b03cd201debb9b6814bf9ed8faed40e5098cbf
alexoviedo999 <alexoviedo999@gmail.com>
2012-10-23 15:37:57 -0400

    Updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121022183955) do
+ActiveRecord::Schema.define(:version => 20121023143622) do
 
   create_table "posts", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20121022183955) do
     t.decimal  "price"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "audiofile"
   end
 
 end
