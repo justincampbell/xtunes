4288bf494f56bc66eaf4d0fbe98fc78832c2e3c6
alexoviedo999 <alexoviedo999@gmail.com>
2012-10-25 00:56:15 -0400

    Added users and purchases controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,33 @@
 Xtunes::Application.routes.draw do
 
+  get "purchases/index"
+
+  get "purchases/new"
+
+  get "purchases/create"
+
+  get "purchases/show"
+
+  get "purchases/edit"
+
+  get "purchases/update"
+
+  get "purchases/destroy"
+
+  get "users/index"
+
+  get "users/new"
+
+  get "users/create"
+
+  get "users/show"
+
+  get "users/edit"
+
+  get "users/update"
+
+  get "users/destroy"
+
   resources :posts
   resources :songs
   resources :artists
