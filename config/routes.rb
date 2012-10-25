d50ec9861832741ce9d0b7364503ae19c6fae6d3
alexoviedo999 <alexoviedo999@gmail.com>
2012-10-25 14:11:12 -0400

    Replacing standard rails index page with my home#index to list songs for purchase

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Xtunes::Application.routes.draw do
 
   resources :posts
-  resources :songs
   resources :artists
   resources :albums
   resources :genres
   resources :purchases
   resources :users
 
+  resources :songs
+
 
   resources :home do
     get 'song'
   end
 
+  root :to => 'home#index'
 
 
   # You can have the root of your site routed with "root"
