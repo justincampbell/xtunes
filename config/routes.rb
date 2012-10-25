49fa9a591ecee5854a4dcaeb2a4e272d95fc41d7
alexoviedo999 <alexoviedo999@gmail.com>
2012-10-25 09:06:17 -0400

    Replacing a bunch of specific routes with a more resourceful approach

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,12 @@
 Xtunes::Application.routes.draw do
 
-  get "purchases/index"
-
-  get "purchases/new"
-
-  get "purchases/create"
-
-  get "purchases/show"
-
-  get "purchases/edit"
-
-  get "purchases/update"
-
-  get "purchases/destroy"
-
-  get "users/index"
-
-  get "users/new"
-
-  get "users/create"
-
-  get "users/show"
-
-  get "users/edit"
-
-  get "users/update"
-
-  get "users/destroy"
-
   resources :posts
   resources :songs
   resources :artists
   resources :albums
   resources :genres
+  resources :purchases
+  resources :users
 
 
   # You can have the root of your site routed with "root"
