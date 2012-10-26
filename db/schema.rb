173b559404a1d6bacd85557c633308022bfc9323
alexoviedo999 <alexoviedo999@gmail.com>
2012-10-26 15:47:54 -0400

    Added password fields to user db field.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121025131935) do
+ActiveRecord::Schema.define(:version => 20121026192814) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -62,8 +62,11 @@ ActiveRecord::Schema.define(:version => 20121025131935) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.string   "password"
+    t.string   "password_hash"
+    t.string   "password_salt"
   end
 
 end
